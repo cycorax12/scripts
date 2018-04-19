e9a05b0f9c95ae3c7245890bcbd2c3987fe0ecf5
cycorax12 <oswalvirendra1@gmail.com>
2018-04-19 14:28:17 +0530

    Update rename_file.cmd

--- rename_file.cmd
+++ rename_file.cmd
@@ -5,7 +5,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 REM ren "C:\Virendra\batch scripts\2009101_67070_deal ticket(apple)_2009120000005.csv" "2009101_67070_dealticket_2009120000005.csv"
 REM ren "C:\Virendra\batch scripts\2009101_67070_deal ticket(apple)_2009120000005.csv" "2009101_67070_dealticket_2009120000005.csv"
 
-REM below commmand loops over each  file in given directory, filters *.csv file and renames
+REM below commmand loops over each  file in given directory, filters *.csv file and calls RenameFile Subroutine
 for %%a in (C:\Virendra\batch-scripts\*.csv) do  (
 	REM ren "%%a" "%finalName%"
 	set test=
@@ -19,7 +19,7 @@ for %%a in (C:\Virendra\batch-scripts\*.csv) do  (
 goto:End
 
 
-
+REM This sub-routine returns file name by removing space and values between round brackets inclusive of it.
 :RenameFile
     set fileName=%1
 	set actualFile=%1
@@ -33,16 +33,23 @@ goto:End
 		
 	pause
 	REM SET "fileName=20090101_67070_deal tic	ket(apple)_2009120000005.csv"
-	for /f "tokens=1 delims=()" %%i in ("%fileName%") do (set prefix=%%i)
+	
+	
 	ECHO We're working with "%fileName%"
+	REM removes space
 	set fileName=%fileName: =%
 	ECHO Removing space to get "%fileName%"
 	pause
 	
+	REM gets prefix before delimiter ()
 	for /f "tokens=1 delims=()" %%i in ("%fileName%") do (set prefix=%%i)
 	ECHO prefix is "%prefix%"
+	
+	REM gets suffix after last _ token
 	for /f "tokens=4 delims=_" %%i in ("%fileName%") do (set suffix=%%i)
 	ECHO suffix is "%suffix%"
+	
+	REM gets final name
 	set finalName=%prefix%_%suffix%
 	echo FinalName "%finalName%"
 	set %2=%finalName%
@@ -50,7 +57,7 @@ goto:End
 :END 
 
 
-
+REM Below sub-routine extracts file name from absolute windows path. It runs for loop to find last delimiter for file.path (\)
 :getlasttoken
 set "check=%~1"
 :loop
